c9a554a69857959d487efea8be0089f0d20fdeb0
Tatsuro Shibamura <me@shibayan.jp>
2022-01-16 21:22:58 +0900

    Adding test cases

---
+++ WinQuickLook.Core/Controls/GenericFileControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace WinQuickLook.Controls;
+
+/// <summary>
+/// GenericFileControl.xaml の相互作用ロジック
+/// </summary>
+public partial class GenericFileControl : UserControl
+{
+    public GenericFileControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ WinQuickLook.Tests/Handlers/HtmlFileQuickLookHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+using WinQuickLook.Handlers;
+
+using Xunit;
+
+namespace WinQuickLook.Tests.Handlers;
+
+public class HtmlFileQuickLookHandlerTests
+{
+    [Theory]
+    [InlineData("test.htm", true)]
+    [InlineData("test.html", true)]
+    [InlineData("test.xhtml", true)]
+    [InlineData("test.HTM", true)]
+    [InlineData("test.HTML", true)]
+    [InlineData("test.XHTML", true)]
+    [InlineData("test.jpg", false)]
+    [InlineData("test.JPG", false)]
+    [InlineData("test", false)]
+    public void CanOpen(string fileName, bool expected)
+    {
+        var fileInfo = new FileInfo(fileName);
+
+        var handler = new HtmlFileQuickLookHandler();
+
+        var actual = handler.CanOpen(fileInfo);
+
+        Assert.Equal(expected, actual);
+    }
+}
