1127373442a4ded6a29468cc8821d22000e0af3f
Tatsuro Shibamura <me@shibayan.jp>
2022-01-19 22:26:33 +0900

    Adding `IPreviewHandler` host implementation

--- WinQuickLook.Core/Controls/PreviewHostControl.cs
+++ WinQuickLook.Core/Controls/PreviewHostControl.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Interop;
+
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.Shell.PropertiesSystem;
 
 namespace WinQuickLook.Controls;
 
@@ -9,4 +16,120 @@ public class PreviewHostControl : HwndHost
     protected override HandleRef BuildWindowCore(HandleRef hwndParent) => throw new NotImplementedException();
 
     protected override void DestroyWindowCore(HandleRef hwnd) => throw new NotImplementedException();
+
+    private IPreviewHandler? _previewHandler;
+
+    public bool Open(FileInfo fileInfo)
+    {
+        if (!TryGetPreviewHandlerCLSID(fileInfo, out var clsid))
+        {
+            return false;
+        }
+
+        if (!TryCreatePreviewHandler(clsid, out var previewHandler))
+        {
+            return false;
+        }
+
+        if (!TryInitializePreviewHandler(previewHandler, fileInfo))
+        {
+            UnloadPreviewHandler(previewHandler);
+
+            return false;
+        }
+
+        var rect = new RECT { left = 0, top = 0, right = (int)ActualWidth, bottom = (int)ActualHeight };
+
+        previewHandler.SetWindow(new HWND(Handle.ToInt32()), rect);
+        previewHandler.DoPreview();
+
+        if (_previewHandler is not null)
+        {
+            UnloadPreviewHandler(_previewHandler);
+
+            _previewHandler = previewHandler;
+        }
+
+        return true;
+    }
+
+    private static unsafe bool TryGetPreviewHandlerCLSID(FileInfo fileInfo, out Guid clsid)
+    {
+        var pcchOut = 40u;
+        var pszOut = stackalloc char[(int)pcchOut];
+
+        var result = PInvoke.AssocQueryString(0x00000004, ASSOCSTR.ASSOCSTR_SHELLEXTENSION, fileInfo.Extension, typeof(IPreviewHandler).GUID.ToString("B"), new PWSTR(pszOut), ref pcchOut);
+
+        if (result.Value < 0)
+        {
+            clsid = Guid.Empty;
+
+            return false;
+        }
+
+        clsid = Guid.Parse(new ReadOnlySpan<char>(pszOut, (int)pcchOut));
+
+        return true;
+    }
+
+    private static bool TryCreatePreviewHandler(Guid clsid, [NotNullWhen(true)] out IPreviewHandler? previewHandler)
+    {
+        var comType = Type.GetTypeFromCLSID(clsid);
+
+        if (comType is null)
+        {
+            previewHandler = null;
+
+            return false;
+        }
+
+        previewHandler = (IPreviewHandler?)Activator.CreateInstance(comType);
+
+        return previewHandler is not null;
+    }
+
+    private static unsafe bool TryInitializePreviewHandler(IPreviewHandler previewHandler, FileInfo fileInfo)
+    {
+        var initializeWithFile = (IInitializeWithFile?)previewHandler;
+
+        if (initializeWithFile is not null)
+        {
+            var result = initializeWithFile.Initialize(fileInfo.FullName, 0);
+
+            return result.Value >= 0;
+        }
+
+        var initializeWithItem = (IInitializeWithItem?)previewHandler;
+
+        if (initializeWithItem is not null)
+        {
+            PInvoke.SHCreateItemFromParsingName(fileInfo.FullName, null, typeof(IShellItem).GUID, out var ppv);
+
+            var shellItem = (IShellItem)Marshal.GetUniqueObjectForIUnknown(new IntPtr(ppv));
+
+            var result = initializeWithItem.Initialize(shellItem, 0);
+
+            Marshal.ReleaseComObject(shellItem);
+
+            return result.Value >= 0;
+        }
+
+        return false;
+    }
+
+    private static void UnloadPreviewHandler(IPreviewHandler previewHandler)
+    {
+        try
+        {
+            previewHandler.Unload();
+        }
+        catch
+        {
+            // ignored
+        }
+        finally
+        {
+            Marshal.FinalReleaseComObject(previewHandler);
+        }
+    }
 }
