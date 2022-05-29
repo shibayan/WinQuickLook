226de6195089e6f6807f101fe41ba0f00616ba76
Tatsuro Shibamura <me@shibayan.jp>
2022-05-29 18:06:01 +0900

    Implement viewmodel commands

---
+++ WinQuickLook.App/DelegateCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WinQuickLook.App;
+
+public class DelegateCommand : ICommand
+{
+    public DelegateCommand(Action execute)
+        : this(execute, () => true)
+    {
+    }
+
+    public DelegateCommand(Action execute, Func<bool> canExecute)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    private readonly Action _execute;
+    private readonly Func<bool> _canExecute;
+
+    public bool CanExecute(object? parameter) => _canExecute();
+
+    public void Execute(object? parameter) => _execute();
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+}
+
+public class DelegateCommand<T> : ICommand
+{
+    public DelegateCommand(Action<T?> execute)
+        : this(execute, _ => true)
+    {
+    }
+
+    public DelegateCommand(Action<T?> execute, Func<T?, bool> canExecute)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    private readonly Action<T?> _execute;
+    private readonly Func<T?, bool> _canExecute;
+
+    public bool CanExecute(object? parameter) => _canExecute((T?)parameter);
+
+    public void Execute(object? parameter) => _execute((T?)parameter);
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+}
--- WinQuickLook.App/ViewModels/NotifyIconViewModel.cs
+++ WinQuickLook.App/ViewModels/NotifyIconViewModel.cs
@@ -1,5 +1,33 @@
-﻿namespace WinQuickLook.App.ViewModels;
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WinQuickLook.App.ViewModels;
 
 internal class NotifyIconViewModel
 {
+    public NotifyIconViewModel()
+    {
+        ActivateCommand = new DelegateCommand(Activate);
+        LaunchOnLoginCommand = new DelegateCommand<bool>(LaunchOnLogin);
+        ExitCommand = new DelegateCommand(Exit);
+    }
+
+    public ICommand ActivateCommand { get; }
+
+    public ICommand LaunchOnLoginCommand { get; }
+
+    public ICommand ExitCommand { get; }
+
+    private void Activate()
+    {
+    }
+
+    private void LaunchOnLogin(bool isChecked)
+    {
+    }
+
+    private void Exit()
+    {
+        Application.Current.Shutdown();
+    }
 }
