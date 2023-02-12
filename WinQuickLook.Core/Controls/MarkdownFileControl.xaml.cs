b1b6b49e009db8d5e927bb9ab8d718f19f40571d
Tatsuro Shibamura <me@shibayan.jp>
2023-02-13 01:59:54 +0900

    Adding better markdown support

---
+++ WinQuickLook.Core/Controls/MarkdownFileControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+using ICSharpCode.AvalonEdit.Highlighting;
+
+using Microsoft.Web.WebView2.Core;
+
+using WinQuickLook.Extensions;
+
+namespace WinQuickLook.Controls;
+
+public partial class MarkdownFileControl
+{
+    public MarkdownFileControl() => InitializeComponent();
+
+    public void Open(FileInfo fileInfo)
+    {
+        textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(fileInfo.Extension);
+        textEditor.Load(fileInfo.OpenReadNoLock());
+    }
+
+    private void WebView2_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+    {
+        webView2.NavigateToString(Markdig.Markdown.ToHtml(textEditor.Text));
+    }
+}
