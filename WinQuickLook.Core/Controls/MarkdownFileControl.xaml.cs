93b64e3ec44295b5f8978c27a9256b185fb9e7bb
Tatsuro Shibamura <me@shibayan.jp>
2023-02-13 20:59:59 +0900

    Improvement Markdown highlighting and preview

--- WinQuickLook.Core/Controls/MarkdownFileControl.xaml.cs
+++ WinQuickLook.Core/Controls/MarkdownFileControl.xaml.cs
@@ -12,10 +12,12 @@ public partial class MarkdownFileControl
 {
     public MarkdownFileControl() => InitializeComponent();
 
-    public void Open(FileInfo fileInfo)
+    public async void Open(FileInfo fileInfo)
     {
-        textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(fileInfo.Extension);
+        textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("MarkDown");
         textEditor.Load(fileInfo.OpenReadNoLock());
+
+        await webView2.EnsureCoreWebView2Async();
     }
 
     private void WebView2_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
